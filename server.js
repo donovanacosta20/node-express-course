68963b9bf0534dd5504c61c4893b189912e569d6
Donovan <dnvnacosta81@gmail.com>
2021-12-16 19:45:32 -0300

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
+
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Jill'}
+]
+
+
+app.get('/users', function(){
+    res.json({
+        sucess: true,
+        massage: 'Succesfully got users. Nice!',
+        users: mockUserData
+    })
+})
 
 app.listen(8000, function(){
     console.log("sever is running");
 });
+
