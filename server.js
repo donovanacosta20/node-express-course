6addbe9c76e030ab26332a17d8e957026f1d2010
Donovan <dnvnacosta81@gmail.com>
2021-12-17 10:44:34 -0300

    add login POST route

--- server.js
+++ server.js
@@ -1,33 +1,60 @@
 const express = require('express');
-const res = require('express/lib/response');
 const app = express();
+
+const bodyParser = require('body-parser');
+const res = require('express/lib/response');
+app.use(bodyParser.json());
+
 
 const mockUserData = [
     {name: 'Mark'},
     {name: 'Jill'}
-]
+];
 
 
 app.get('/users', function(req, res){
     res.json({
-        sucess: true,
+        success: true,
         massage: 'Succesfully got users. Nice!',
         users: mockUserData
-    })
-})
+    });
+});
 
 
 app.get('/users/:id', function(req, res){
 
     console.log(req.params.id);
     res.json({
-        sucess: true,
+        success: true,
         message: 'get one user',
         user: req.params.id
         
     })
-})
+});
+
+
+app.post('/login', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if(username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        });
+    }else {
+        res.json({
+            success: false,
+            message: 'password and username do not match!'
+        });
+    }
+});
 
 app.listen(8000, function(){
     console.log("sever is running");
 });
+
